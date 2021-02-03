be6e431f52f7cad5f371de1b138cc0a6c9d1f672
4kir <tkakenov@mail.ru>
2021-02-04 01:06:26 +0600

    Add Feature#View alien game

---
+++ spec/features/user_viewing_alien_game_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature 'USER viewing alien game', type: :feature do
+  let(:timur) { FactoryBot.create :user, name: 'Тимур', balance: 1000 }
+  let(:sergey) { FactoryBot.create :user, name: 'Сергей', balance: 5000 }
+  let!(:game) { FactoryBot.create(:game, id: 15, created_at: Time.parse('2016.10.09, 13:00'), finished_at: Time.parse('2016.10.09, 13:30'), is_failed: true, current_level: 15, prize: 1000, user: sergey) }
+  let!(:game1) { FactoryBot.create(:game, id: 19, created_at: Time.parse('2016.10.09, 18:00'), current_level: 7, prize: 2000, user: sergey) }
+
+  before(:each) do
+    login_as timur
+  end
+
+  scenario 'successfully' do
+
+    visit '/'
+
+    click_link 'Сергей'
+
+    visit '/users/1'
+
+    expect(page).not_to have_content 'Сменить имя и пароль'
+    expect(page).to have_content 'Сергей'
+    expect(page).to have_content 'в процессе'
+    expect(page).to have_content 'проигрыш'
+    expect(page).to have_content '1 000 ₽'
+    expect(page).to have_content '2 000 ₽'
+    expect(page).to have_content '09 окт., 13:00'
+    expect(page).to have_content '09 окт., 18:00'
+  end
+end
