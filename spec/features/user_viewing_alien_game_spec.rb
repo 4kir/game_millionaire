e2cd759d22d72556c7104238eed1cab25f51e3f5
4kir <tkakenov@mail.ru>
2021-02-11 15:34:06 +0600

    Fix Feature#view test

--- spec/features/user_viewing_alien_game_spec.rb
+++ spec/features/user_viewing_alien_game_spec.rb
@@ -4,8 +4,8 @@ RSpec.feature 'USER viewing alien game', type: :feature do
   let(:timur) { FactoryBot.create :user, name: 'Тимур', balance: 1000 }
   let(:sergey) { FactoryBot.create :user, name: 'Сергей', balance: 5000 }
   let!(:game) do [
-      FactoryBot.create(:game, id: 15, created_at: Time.parse('2016.10.09, 13:00'), finished_at: Time.parse('2016.10.09, 13:30'), is_failed: true, current_level: 15, prize: 1000, user: sergey),
-      FactoryBot.create(:game, id: 19, created_at: Time.parse('2016.10.09, 18:00'), current_level: 7, prize: 2000, user: sergey) 
+      FactoryBot.create(:game, created_at: Time.parse('2016.10.09, 13:00'), finished_at: Time.parse('2016.10.09, 13:30'), is_failed: true, current_level: 15, prize: 1000, user: sergey),
+      FactoryBot.create(:game, created_at: Time.parse('2016.10.09, 18:00'), current_level: 7, prize: 2000, user: sergey) 
     ]
   end
 
@@ -16,7 +16,7 @@ RSpec.feature 'USER viewing alien game', type: :feature do
   scenario 'successfully' do
     visit '/'
     click_link 'Сергей'
-
+    expect(page).to have_current_path '/users/1'
     expect(page).not_to have_content 'Сменить имя и пароль'
     expect(page).to have_content 'Сергей'
     expect(page).to have_content 'в процессе'
